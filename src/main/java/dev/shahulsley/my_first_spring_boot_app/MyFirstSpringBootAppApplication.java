d901087e16f4040bae09b86d23e04590b8f384aa
Shahul Hameed <dev.shahulsley@outlook.com>
2024-07-24 22:12:47 -0400

    Initial Commit - Newly generated spring boot app

---
+++ src/main/java/dev/shahulsley/my_first_spring_boot_app/MyFirstSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package dev.shahulsley.my_first_spring_boot_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSpringBootAppApplication.class, args);
+	}
+
+}
